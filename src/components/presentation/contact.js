0eff5a003e4840f5c3b697a12ad482f1e8cbcd4b
neha singh <nehasingh.apr12@gmail.com>
2020-10-30 19:02:27 +0530

    react code

--- src/components/presentation/contact.js
+++ src/components/presentation/contact.js
@@ -1,31 +1,17 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
-import { connect } from 'react-redux'
 
-import {fieldCd, skinCodes}  from '../../constants/typeCodes';
+import {fieldCd}  from '../../constants/typeCodes';
 
 import ResumePreview from './resumePreview'
 
   class Contact extends React.Component {
   constructor(props, context) {
     super(props, context);
-            this.state = {
-                educationSection: {},
-                contactSection: {},
-                skinCd:'skin1'
-          };      
+            this.state = {};      
     }
-    
-    onChange=(event)=>{
-        this.setState({contactSection:{...this.state.contactSection, [event.target.name]: event.target.value}})
-    }
-    onSubmit=()=>{
-       this.props.history.push('/education')
-    }
-
 
   render() { 
-    const { contactSection ,educationSection} = this.state
     return (
           <div className="container med contact">
             <div className="section funnel-section">
@@ -33,67 +19,66 @@ import ResumePreview from './resumePreview'
                     <h2 className="form-heading center">Personal Details</h2>
                     <div className="form-section">
                         <div className="input-group"><label>First Name</label>
-                            <div className="effect"><input type="text" name={fieldCd.FirstName}
-                                    onChange={this.onChange}  value={contactSection[fieldCd.FirstName]}/><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.FirstName}      /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>Last Name</label>
-                            <div className="effect"><input type="text" name={fieldCd.LastName}  onChange={this.onChange} value={contactSection[fieldCd.LastName]} /><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.LastName}/><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group full"><label>Professional Summary</label>
-                            <div className="effect"><input type="text" name={fieldCd.ProfSummary} onChange={this.onChange} value={contactSection[fieldCd.ProfSummary]} /><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.ProfSummary} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>Email</label>
-                            <div className="effect"><input type="text"  name={fieldCd.Email} onChange={this.onChange}  value={contactSection[fieldCd.Email]} /><span></span>
+                            <div className="effect"><input type="text"  name={fieldCd.Email}  /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>Phone</label>
-                            <div className="effect"><input type="text"  name={fieldCd.Phone} onChange={this.onChange}  value={contactSection[fieldCd.Phone]}/><span></span>
+                            <div className="effect"><input type="text"  name={fieldCd.Phone} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>Profession</label>
-                            <div className="effect"><input type="text"  name={fieldCd.Profession} onChange={this.onChange}  value={contactSection[fieldCd.Profession]} /><span></span>
+                            <div className="effect"><input type="text"  name={fieldCd.Profession} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
                         <div className="input-group"><label>Street</label>
-                            <div className="effect"><input type="text" name={fieldCd.Street} onChange={this.onChange}  value={contactSection[fieldCd.Street]} /><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.Street}  /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>City</label>
-                            <div className="effect"><input type="text" name={fieldCd.City} onChange={this.onChange}   value={contactSection[fieldCd.City]}/><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.City} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>State</label>
-                            <div className="effect"><input type="text"   name={fieldCd.State} onChange={this.onChange}  value={contactSection[fieldCd.State]} /><span></span>
+                            <div className="effect"><input type="text"   name={fieldCd.State}  /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
 
                         <div className="input-group"><label>Country</label>
-                            <div className="effect"><input type="text"  name={fieldCd.Country} onChange={this.onChange}  value={contactSection[fieldCd.Country]}/><span></span>
+                            <div className="effect"><input type="text"  name={fieldCd.Country} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
                         <div className="input-group"><label>Pin Code</label>
-                            <div className="effect"><input type="text" name={fieldCd.ZipCode} onChange={this.onChange}   value={contactSection[fieldCd.ZipCode]}/><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.ZipCode}/><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
@@ -106,7 +91,7 @@ import ResumePreview from './resumePreview'
                 </div>
 
                 <div className="preview-card">
-                    <ResumePreview skinCd={skinCodes[0]} contactSection={contactSection} educationSection={educationSection}></ResumePreview>
+                    <ResumePreview></ResumePreview>
                 </div>
 
             </div>
--- src/components/presentation/resumePreview.js
+++ src/components/presentation/resumePreview.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import {skinCodes, fieldCd} from './../../constants/typeCodes'
+import {fieldCd} from './../../constants/typeCodes'
 
 
 class ResumePreview extends React.PureComponent{
-    
     render() {    
-        
-      var  {contactSection, educationSection } = this.props;
-      var name = contactSection[fieldCd.FirstName]+ ' ' + contactSection[fieldCd.LastName];
 
+        console.log("Hello");
         return (
-            <div className={"resume-preview " +this.props.skinCd}>
+            <div className={"resume-preview skin1"}>
                 <div className={'name-section'}>
-                    <p className={'center contact-name text-upper' }> {name} </p>
-                    <p className={'center address'}>{contactSection[fieldCd.Street]+', ' +contactSection[fieldCd.City] + ', ' + contactSection[fieldCd.State]+ ', ' + contactSection[fieldCd.Country]+ ', ' + contactSection[fieldCd.ZipCode]}</p>
-                    <p className={'center'}>{contactSection[fieldCd.Email]}</p>
-                    <p className={'center'}>{contactSection[fieldCd.Phone]}</p>
+                    <p className={'center contact-name text-upper' }> Test Name</p>
+                    <p className={'center address'}></p>
+                    <p className={'center'}></p>
+                    <p className={'center'}></p>
                 </div>
-                <div className={'educationSection text-upper'}>
-                   
+                <div className={'educationSection text-upper'}>                   
                     <p className="heading bold">EDUCATIONAL DETAILS</p>
                      <div className={'divider'}></div>
-                     <p> {educationSection[fieldCd.SchoolName]}</p>
+                     <p></p>
                 </div>
           
             </div>
