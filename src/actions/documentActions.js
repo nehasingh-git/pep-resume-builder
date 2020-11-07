9d7240c044fdc4632b28755e005aa01d1ae99e64
neha singh <nehasingh.apr12@gmail.com>
2020-11-07 19:57:12 +0530

    test

--- src/actions/documentActions.js
+++ src/actions/documentActions.js
@@ -1,26 +1,26 @@
 import * as actionTypes from './actionTypes';
 
-// export const setSkinCd=(skinCd)=>{
-//     return(dispatch,getState,{getFirebase, getFirestore})=>{
+export const setSkinCd=(skinCd)=>{
+    return(dispatch,getState,{getFirebase, getFirestore})=>{
 
-//         const firestore = getFirestore();
-//         firestore.collection('resumes').add({"document":{"id":"1234", "skinCd":skinCd,"createdAt":new Date(), userId:1 }}).then(()=>
-//             dispatch({type: actionTypes.SET_SKIN, skinCd : skinCd})
-//         ).catch((error)=>
-//             dispatch({type: actionTypes.SET_SKIN, skinCd : 'skin2'})
-//         )      
-//     }
+        const firestore = getFirestore();
+        firestore.collection('resumes').add({"document":{"id":"1234", "skinCd":skinCd,"createdAt":new Date(), userId:1 }}).then(()=>
+            dispatch({type: actionTypes.SET_SKIN, skinCd : skinCd})
+        ).catch((error)=>
+            dispatch({type: actionTypes.SET_SKIN, skinCd : 'skin2'})
+        )      
+    }
    
-// }
+}
  
 
-export function setSkinCd(skinCd){
+// export function setSkinCd(skinCd){
 
-    return(dispatch, getState )=>{
-        return {type: actionTypes.SET_SKIN, skinCd : skinCd}
-    }
+//     return(dispatch, getState )=>{
+//         return {type: actionTypes.SET_SKIN, skinCd : skinCd}
+//     }
     
-}
+// }
 
 export function updateSkinCd(skinCd){
     return {type: actionTypes.UPDATE_SKIN, skinCd : skinCd}
