cc56f731879bad894b45b4927998b05fd21d13ec
nehasingh-git <nehasingh.apr12@gmail.com>
2020-12-04 20:44:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,13 +15,7 @@ import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 import { createFirestoreInstance } from 'redux-firestore';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyD3WovTd1W6osCZzx-EU8IST3NGtPBUUrI",
-  authDomain: "pep-resume-api1.firebaseapp.com",
-  databaseURL: "https://pep-resume-api1.firebaseio.com",
-  projectId: "pep-resume-api1",
-  storageBucket: "pep-resume-api1.appspot.com",
-  messagingSenderId: "1015571652091",
-  appId: "1:1015571652091:web:3cca0bb1c4c77ddba9bf5e"
+   
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
